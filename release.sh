c18ff3809cb3509d591ce5cc6290d667b0d14d51
Gitlab CI <ruben.malchow@gmail.com>
2021-07-05 19:28:51 +0200

    release prepare

--- release.sh
+++ release.sh
@@ -2,7 +2,6 @@ git add . && git commit -m "release prepare" && git push
 git checkout master
 git merge develop
 git add . && git commit -m "release prepare" && git push
-mvn --batch-mode -s /Users/rm/Eclipse/m2/skjlls/settings-home.xml -Dresume=false release:prepare release:perform
+mvn --batch-mode -s settings.xml -Dresume=false release:prepare release:perform
 git checkout develop
 git merge master
-
